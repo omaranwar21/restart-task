0bb1d573030ade3474e7614a6205da3e13733615
Omar Anwar <omar.mansour00@eng-st.cu.edu.eg>
2025-07-30 16:23:11 +0300

    fix: update TopLoader color and shadow properties for consistency

--- src/components/common/TopLoader.tsx
+++ src/components/common/TopLoader.tsx
@@ -6,7 +6,7 @@ import NextTopLoader from "nextjs-toploader";
 export default function TopLoader() {
   return (
     <NextTopLoader
-      color="#2657eb" // Primary blue color
+      color="#2c3e50" // Primary gray color
       initialPosition={0.08}
       crawlSpeed={200}
       height={6}
@@ -14,8 +14,8 @@ export default function TopLoader() {
       showSpinner={false}
       easing="ease"
       speed={200}
-      shadow="0 0 10px #2657eb,0 0 5px #2657eb"
-      zIndex={1600}
+      shadow="0 0 10px #bdc3c7,0 0 5px #bdc3c7"
+      zIndex={1700}
       showAtBottom={false}
     />
   );
